cda8417fc5242005d7557f2ef6a6aa5cf59514f3
Iulian Peiu <iulianpeiu6@gmail.com>
2021-10-31 02:06:15 +0300

    Test

---
+++ WatchsterApplication/Watchster.UnitTests/Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Watchster.UnitTests
+{
+    [TestFixture]
+    public class Test
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [Test]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            Assert.Fail();
+        }
+    }
+}
--- WatchsterApplication/Watchster.WebApi/Program.cs
+++ WatchsterApplication/Watchster.WebApi/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Hosting;
 
 namespace Watchster.WebApi
 {
-    public static class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
