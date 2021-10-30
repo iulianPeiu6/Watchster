fae987e2b8006fd748254fddcb3f732cabdb52aa
Iulian Peiu <iulianpeiu6@gmail.com>
2021-10-30 13:31:26 +0300

    Create TestConsole for IntegrationTests

---
+++ WatchsterApplication/Watchster.TestConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Watchster.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Running Watchster Integration Tests");
+
+            Console.WriteLine("Finished Running Watchster Integration Tests");
+        }
+    }
+}
