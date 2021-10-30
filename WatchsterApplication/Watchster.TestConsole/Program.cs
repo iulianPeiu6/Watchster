1fa7746e8a95728e656757923e7f5efb7647d4db
Iulian Peiu <iulianpeiu6@gmail.com>
2021-10-30 17:02:10 +0300

    Init Proj & Config Sonar
    Create .gitignore
    Create Blank Solution
    Create a TestConsole Project for IntegrationTests
    Create WorkflowAction for Sonar

---
+++ WatchsterApplication/Watchster.TestConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Watchster.TestConsole
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Start Running Watchster Integration Tests");
+
+            Console.WriteLine("Finished Running Watchster Integration Tests");
+        }
+    }
+}
