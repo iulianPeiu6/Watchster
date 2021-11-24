e16c533cfe967a1749da4c99d87c9de415d3a11e
Iulian Peiu <iulianpeiu6@gmail.com>
2021-11-24 23:48:44 +0200

    Implement MovieRecommender Service
    Add Dataset
    Add Models
    Add DI Support
    Create MovieRecommender Engine
    Evaluate Model
    Implement Save Model
    Implement Load Model
    Run Integration Tests

--- WatchsterSolution/Watchster.MLUtil.TestConsole/Program.cs
+++ WatchsterSolution/Watchster.MLUtil.TestConsole/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Watchster.MLUtil.Models;
+using Watchster.MLUtil.Services.Interfaces;
 
 namespace Watchster.MLUtil.TestConsole
 {
-    static class Program
+    class Program
     {
         static void Main()
         {
-            throw new NotImplementedException();
+            var services = new ServiceCollection()
+                .AddMLUtil()
+                .AddLogging(configure => configure.AddConsole())
+                .Configure<LoggerFilterOptions>(options => options.MinLevel = LogLevel.Information)
+                .BuildServiceProvider();
+
+            TestMovieRecommender(services);
+        }
+
+        private static void TestMovieRecommender(ServiceProvider services)
+        {
+            var movieRecommender = services.GetRequiredService<IMovieRecommender>();
+
+            var logger = services.GetRequiredService<ILogger<Program>>();
+
+            var movieRatingToPredict = new MovieRating()
+            {
+                UserId = 1,
+                MovieId = 7438,
+            };
+
+            var moviePrediction = movieRecommender.PredictMovieRating(movieRatingToPredict);
+            logger.LogInformation($"{moviePrediction.Score}");
         }
     }
 }
