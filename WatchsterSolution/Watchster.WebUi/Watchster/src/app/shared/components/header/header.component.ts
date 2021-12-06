69afff122ba857f1b4970fbe4cbb43ae1b51756e
gmatei <mateicosmingoanta@gmail.com>
2021-12-06 17:17:51 +0200

    Added user data saving for user authentication

--- WatchsterSolution/Watchster.WebUi/Watchster/src/app/shared/components/header/header.component.ts
+++ WatchsterSolution/Watchster.WebUi/Watchster/src/app/shared/components/header/header.component.ts
@@ -23,7 +23,7 @@ export class HeaderComponent implements OnInit {
   @Input()
   title!: string;
 
-  user: IUser | null = { email: '' };
+  user: IUser | null = { email: '', id: '', token: '', isSubscribed: true };
 
   userMenuItems = [{
     text: 'Profile',
