6b500c274becbb81e5513711f0205208e4e5d57d
Iulian Peiu <iulianpeiu6@gmail.com>
2021-12-13 03:44:34 +0200

    Add GetAllRatings Query & QueryHandler. Refactoring

---
+++ WatchsterSolution/Watchster.Application/Features/Queries/GetAllRatingsQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Watchster.Application.Interfaces;
+using Watchster.Domain.Entities;
+
+namespace Watchster.Application.Features.Queries
+{
+    public class GetAllRatingsQueryHandler : IRequestHandler<GetAllRatingsQuery, IEnumerable<Rating>>
+    {
+        private readonly IRatingRepository repository;
+
+        public GetAllRatingsQueryHandler(IRatingRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<IEnumerable<Rating>> Handle(GetAllRatingsQuery request, CancellationToken cancellationToken)
+        {
+            return await repository.GetAllAsync();
+        }
+    }
+}
--- WatchsterSolution/Watchster.Application/Interfaces/IRatingRepository.cs
+++ WatchsterSolution/Watchster.Application/Interfaces/IRatingRepository.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Watchster.Aplication.Interfaces;
+﻿using Watchster.Aplication.Interfaces;
 using Watchster.Domain.Entities;
 
 namespace Watchster.Application.Interfaces
