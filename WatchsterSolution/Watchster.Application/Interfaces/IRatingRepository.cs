c40df802a0df2beacdd506b43ed82c81a8c5fb6c
Iulian Peiu <iulianpeiu6@gmail.com>
2021-12-13 03:38:04 +0200

    Add Rating Repository

---
+++ WatchsterSolution/Watchster.Application/Interfaces/IRatingRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Watchster.Aplication.Interfaces;
+using Watchster.Domain.Entities;
+
+namespace Watchster.Application.Interfaces
+{
+    public interface IRatingRepository : IRepository<Rating>
+    {
+    }
+}
---
+++ WatchsterSolution/Watchster.DataAccess/Repositories/RatingRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Watchster.Application.Interfaces;
+using Watchster.DataAccess.Context;
+using Watchster.Domain.Entities;
+
+namespace Watchster.DataAccess.Repositories
+{
+    internal class RatingRepository : Repository<Rating>, IRatingRepository
+    {
+        public RatingRepository(WatchsterContext context) : base(context)
+        {
+        }
+    }
+}
