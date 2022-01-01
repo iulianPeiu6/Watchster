6961457835bf440d8ce1edd37be4e929febae1b9
Iulian Peiu <iulianpeiu6@gmail.com>
2022-01-01 16:03:18 +0200

    Refactoring. Write GetAllMoviesQuery tests

---
+++ WatchsterSolution/Watchster.Application.UnitTests/Features/Queries/GetAllMoviesTests.cs
@@ -0,0 +1,36 @@
+﻿using FakeItEasy;
+using Faker;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Watchster.Application.Features.Queries;
+using Watchster.Application.Interfaces;
+using Watchster.Application.Models;
+using Watchster.Application.Utils.Cryptography;
+
+namespace Watchster.Application.UnitTests.Features.Queries
+{
+    public class GetAllMoviesTests
+    {
+        private readonly GetAllMoviesQueryHandler handler;
+        private readonly IMovieRepository movieRepository;
+
+        public GetAllMoviesTests()
+        {
+            movieRepository = A.Fake<IMovieRepository>();
+            handler = new GetAllMoviesQueryHandler(movieRepository);
+        }
+
+        [Test]
+        public async Task Given_GetAllMoviesQuery_When_GetAllMoviesQueryHandlerIsCalled_Should_GetAllAsyncIsCalledAsync()
+        {
+            var query = new GetAllMoviesQuery();
+
+            var response = await handler.Handle(query, default);
+
+            A.CallTo(() => movieRepository.GetAllAsync()).MustHaveHappenedOnceExactly();
+        }
+    }
+}
