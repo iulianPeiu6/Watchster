0b2de6bad29c53896c1c9537fd8f10025e5f802f
Iulian Peiu <iulianpeiu6@gmail.com>
2022-01-01 06:10:16 +0200

    Add tests for MLUtil DI

---
+++ WatchsterSolution/Watchster.Application.UnitTests/Utils/ML/MLUtilServiceCollectionExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
+using System.Reflection;
+using Watchster.Application.Interfaces;
+using Watchster.Application.Utils.ML;
+
+namespace Watchster.Application.UnitTests.Utils.ML
+{
+    public class MLUtilServiceCollectionExtensionsTests
+    {
+        private IServiceCollection services;
+        private IServiceProvider serviceProvider;
+
+        public MLUtilServiceCollectionExtensionsTests()
+        {
+            services = A.Fake<IServiceCollection>();
+        }
+
+        [Test]
+        public void Given_Services_When_AddMLUtil_Should_PutMlUtilServicesInServiceCollection()
+        {
+            var resultServices = services.AddMLUtil();
+
+            resultServices.Should().NotBeNull();
+            resultServices.Should().BeSameAs(services);
+            //A.CallTo(() => services.AddTransient<IMovieRecommender, IMovieRecommender>()).MustHaveHappenedOnceExactly();
+        }
+    }
+}
