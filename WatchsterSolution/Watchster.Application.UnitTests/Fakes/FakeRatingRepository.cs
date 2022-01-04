f14fa3333cb0c3f40ac8468c8d771ef9e4ae3d19
PrEaDiVviN <75678649+PrEaDiVviN@users.noreply.github.com>
2022-01-04 23:09:15 +0200

    Add UnitTest for ChangePasswordCode

---
+++ WatchsterSolution/Watchster.Application.UnitTests/Fakes/FakeRatingRepository.cs
@@ -0,0 +1,110 @@
+﻿using Faker;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Watchster.Application.Interfaces;
+using Watchster.Domain.Entities;
+
+namespace Watchster.Application.UnitTests.Fakes
+{
+    public class FakeRatingRepository : IRatingRepository, IQueryable<Rating>
+    {
+        private List<Rating> entities = null;
+
+        public FakeRatingRepository(IEnumerable<Rating> collection)
+        {
+            this.entities = new List<Rating>(collection);
+        }
+
+        public IEnumerator<Rating> GetEnumerator()
+        {
+            return entities.GetEnumerator();
+        }
+
+
+
+        public Task<Rating> AddAsync(Rating entity)
+        {
+            entities.Add(entity);
+            return Task.Run(() => entity);
+        }
+
+        public Task<Rating> UpdateAsync(Rating entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Rating> Delete(Rating entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Rating>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Rating> GetByIdAsync(int id)
+        {
+            if (id <= 0)
+            {
+                return Task.Run(() => default(Rating));
+            }
+            else
+            {
+                return Task.Run(() => new Rating()
+                {
+                    Id = RandomNumber.Next(),
+                    MovieId = RandomNumber.Next(),
+                    RatingValue = RandomNumber.Next(),
+                    UserId = RandomNumber.Next(),
+                });
+            }
+        }
+
+        public IQueryable<Rating> Query()
+        {
+            throw new NotImplementedException();
+        }
+
+        private object GetValue(MemberExpression member)
+        {
+            var objectMember = Expression.Convert(member, typeof(object));
+
+            var getterLambda = Expression.Lambda<Func<object>>(objectMember);
+
+            var getter = getterLambda.Compile();
+
+            return getter();
+        }
+
+        public IQueryable<Rating> Query(Expression<Func<Rating, bool>> expression)
+        {
+            return entities.Where(expression.Compile()).AsQueryable();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type ElementType
+        {
+            get { return this.entities.AsQueryable().ElementType; }
+        }
+
+        public Expression Expression
+        {
+            get { return this.entities.AsQueryable().Expression; }
+        }
+
+        public IQueryProvider Provider
+        {
+            get { return this.entities.AsQueryable().Provider; }
+        }
+    }
+}
---
+++ WatchsterSolution/Watchster.Application.UnitTests/Features/Commands/ChangeUserPasswordCommandTest.cs
@@ -0,0 +1,110 @@
+﻿using FakeItEasy;
+using Faker;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Watchster.Application.Features.Commands;
+using Watchster.Application.Interfaces;
+using Watchster.Application.Models;
+using Watchster.Application.UnitTests.Fakes;
+
+namespace Watchster.Application.UnitTests.Features.Commands
+{
+    public class ChangeUserPasswordCommandTest
+    {
+        private const int minValue = 1;
+        private const int maxValue = int.MaxValue;
+        private FakeUserRepository userRepository;
+        private FakeResetPasswordCodeRepository resetPasswordCodeRepository;
+        private ICryptographyService cryptography;
+        private ChangeUserPasswordCommandHandler changeUserCommandHandler;
+
+        public ChangeUserPasswordCommandTest()
+        {
+            userRepository = A.Fake<FakeUserRepository>();
+            resetPasswordCodeRepository = A.Fake<FakeResetPasswordCodeRepository>();
+            cryptography = A.Fake<ICryptographyService>();
+            changeUserCommandHandler = new ChangeUserPasswordCommandHandler(userRepository, resetPasswordCodeRepository, cryptography);
+        }
+
+        [Test]
+        public async Task Given_ChangeUserPasswordCommand_When_UserCodeNotFound_Should_FailReturnChangePasswordResult()
+        {
+            //arrage
+            var command = new ChangeUserPasswordCommand()
+            {
+                Code = "",
+                Password = Lorem.Sentence(),
+            };
+
+            //act
+            var response = await changeUserCommandHandler.Handle(command, default);
+
+            //assert
+            response.Should().NotBeNull();
+            response.ErrorMessage.Should().Be(Error.WrongPassChangeCode);
+            response.IsSuccess.Should().Be(false);
+        }
+
+        [Test]
+        public async Task Given_ChangeUserPasswordCommand_When_UserCodeFoundButUserNotFound_Should_FailReturnChangePasswordResult()
+        {
+            //arrage
+            var command = new ChangeUserPasswordCommand()
+            {
+                Code = "code without email",
+            };
+
+            //act
+            var response = await changeUserCommandHandler.Handle(command, default);
+
+            //assert
+            response.Should().NotBeNull();
+            response.ErrorMessage.Should().Be(Error.WrongPassChangeCode);
+            response.IsSuccess.Should().Be(false);
+        }
+
+        [Test]
+        public async Task Given_ChangeUserPasswordCommand_When_UserCodeFoundButUserFoundPasswordNotRespectingContrains_Should_FailReturnChangePasswordResult()
+        {
+            //arrage
+            var command = new ChangeUserPasswordCommand()
+            {
+                Code = "hardcoded",
+                Password = ""
+            };
+
+            //act
+            var response = await changeUserCommandHandler.Handle(command, default);
+
+            //assert
+            response.Should().NotBeNull();
+            response.ErrorMessage.Should().Be(Error.InvalidPass);
+            response.IsSuccess.Should().Be(false);
+        }
+
+        [Test]
+        public async Task Given_ChangeUserPasswordCommand_When_UserCodeFoundButUserFoundPasswordRespectingContrains_Should_FailReturnChangePasswordResult()
+        {
+            //arrage
+            var command = new ChangeUserPasswordCommand()
+            {
+                Code = "hardcoded",
+                Password = "123456"
+            };
+
+            //act
+            var response = await changeUserCommandHandler.Handle(command, default);
+
+            //assert
+            response.Should().NotBeNull();
+            response.ErrorMessage.Should().Be(null);
+            response.IsSuccess.Should().Be(true);
+            A.CallTo(() => cryptography.GetPasswordSHA3Hash(command.Password)).MustHaveHappened();
+        }
+    }
+}
